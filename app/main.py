593013249e8cd9c47854181b818f56df82132f55
DevMehta-30 <dev.a.mehta30@gmail.com>
2024-08-31 21:30:09 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from os import chdir
+from os.path import expanduser
 
 def main():
     # Uncomment this block to pass the first stage
@@ -19,7 +21,7 @@ def main():
                 for path in paths:
                     if os.path.isfile(f"{path}/{s}"):
                         cmd_path = f"{path}/{s}"
-                if s in ["echo","exit","type","pwd"]:
+                if s in ["echo","exit","type","pwd","cd"]:
                     print(f"{s} is a shell builtin")
                 elif cmd_path:
                     print(f"{s} is {cmd_path}")
@@ -27,6 +29,12 @@ def main():
                     print(f"{s}: not found")
             elif command=="pwd":
                 print(os.getcwd())
+            elif command=="cd":
+                d=command[3:]
+                try:
+                    chdir(expanduser(d))
+                except OSError:
+                    print(f"cd: {d}: No such file or directory")
             else:
                 if command.startswith('my_exe'):
                     os.system(command)
