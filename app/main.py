fd016afa0d21d9b3ae93d0c25fd5b77ad10695e4
DevMehta-30 <dev.a.mehta30@gmail.com>
2024-08-31 21:19:40 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,37 +1,31 @@
 import sys
 import os
 
-def find_in_path(param):
-    path = os.environ['PATH']
-    print("Path: " + path)
-    print(f"Param: {param}")
-    for directory in path.split(":"):
-        for (dirpath, dirnames, filenames) in os.walk(directory):
-            if param in filenames:
-                return f"{dirpath}/{param}"
-    return None
-
 def main():
+    # Uncomment this block to pass the first stage
+    PATH=os.environ.get("PATH")
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        command = input()
-        match command.split(" "):
-            case ["exit", "0"]:
-                exit(0)
-            case ["echo", *cmd]:
-                print(" ".join(cmd))
-            case ["type", *cmd]:
-                match cmd:
-                    case ["echo" | "exit" | "type"]:
-                        print(f"${cmd[0]} is a shell builtin")
-                    case _:
-                        location = find_in_path(cmd[0])
-                        if location:
-                            print(f"${cmd[0]} is {location}")
-                        else:
-                            print(f"${" ".join(cmd)} not found")
-            case _:
+        if command := input().strip():
+            if command == "exit 0":
+                sys.exit(0)
+            elif command.startswith("echo "):
+                print(command[len("echo ") :])
+            elif command.startswith("type "):
+                s=command[len("type "):]
+                cmd_path=None
+                paths=PATH.split(":")
+                for path in paths:
+                    if os.path.isfile(f"{path}/{s}"):
+                        cmd_path = f"{path}/{s}"
+                if s in ["echo","exit","type"]:
+                    print(f"{s} is a shell builtin")
+                elif cmd_path:
+                    print(f"{s} is {cmd_path}")
+                else:
+                    print(f"{s}: not found")
+            else:
                 if command.startswith('my_exe'):
                     os.system(command)
                 else:
