faa871f0897da78292b141531e066b6cbe351f24
DevMehta-30 <dev.a.mehta30@gmail.com>
2024-08-31 21:14:48 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,32 +1,37 @@
 import sys
 import os
 
+def find_in_path(param):
+    path = os.environ['PATH']
+    print("Path: " + path)
+    print(f"Param: {param}")
+    for directory in path.split(":"):
+        for (dirpath, dirnames, filenames) in os.walk(directory):
+            if param in filenames:
+                return f"{dirpath}/{param}"
+    return None
+
 def main():
-    # Uncomment this block to pass the first stage
-    path=os.environ["PATH"]
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        if command := input():
-            if command == "exit 0":
-                sys.exit(0)
-            elif command.startswith("echo "):
-                print(command[len("echo ") :])
-            elif command.startswith("type "):
-                s=command[len("type "):]
-                cmd_path=None
-                paths=path.split(":")
-                for p in paths:
-                    if os.path.isfile(f"{p}/{s}"):
-                        cmd_path = f"{p}/{s}"
-                if s in ["echo","exit","type"]:
-                    print(f"{s} is a shell builtin")
-                elif cmd_path:
-                    print(f"{s} is {cmd_path}")
-                else:
-                    print(f"{s}: not found")
-            else:
-                print(path)
+        command = input()
+        match command.split(" "):
+            case ["exit", "0"]:
+                exit(0)
+            case ["echo", *cmd]:
+                print(" ".join(cmd))
+            case ["type", *cmd]:
+                match cmd:
+                    case ["echo" | "exit" | "type"]:
+                        print(f"${cmd[0]} is a shell builtin")
+                    case _:
+                        location = find_in_path(cmd[0])
+                        if location:
+                            print(f"${cmd[0]} is {location}")
+                        else:
+                            print(f"${" ".join(cmd)} not found")
+            case _:
                 if os.path.isfile(command.split(" ")[0]):
                     os.system(command)
                 else:
