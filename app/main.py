0f9d63295e6e98831278772c4fb122753eaf6060
DevMehta-30 <dev.a.mehta30@gmail.com>
2024-08-31 20:08:17 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -3,14 +3,10 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    valid_commands = []
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_command = input()
-        if user_command not in valid_commands:
-            print(f"{user_command}: command not found")
-            continue
         if user_command == "exit 0":
             break
 
