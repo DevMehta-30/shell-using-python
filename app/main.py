555dc5ceadd2f4d9c0815ce17cc5a21b731fc9a5
DevMehta-30 <dev.a.mehta30@gmail.com>
2024-08-31 20:25:15 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -11,6 +11,12 @@ def main():
                 sys.exit(0)
             elif command.startswith("echo "):
                 print(command[len("echo ") :])
+            elif command.startswith("type "):
+                s=command[len("type ")]
+                if s in ["echo","exit","type"]:
+                    print(f"{s} is a shell builtin")
+                else:
+                    print(f"{s} not found")
             else:
                 print(f"{command}: command not found")
 
